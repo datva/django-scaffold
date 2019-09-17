8b83843c3466c753be46b5b4f74dea96afd31add
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-09-17 18:23:30 +0530

    new namespace

---
+++ app/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Orders(models.Model):
+    email = models.TextField()
+    password = models.TextField()
---
+++ app/tests.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from .models import Songs
+from .serializers import SongsSerializer
+
+# tests for views
+
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    @staticmethod
+    def create_order(email=""):
+        if email != "": 
+            Orders.objects.create(email=email)
+
+    def setUp(self):
+        # add test data
+        self.create_order("angadsharma1016@gmail.com")
+
+# Create your tests here.
