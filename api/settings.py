52a43dbaddde68f6b28732ed82fb29ea29436588
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-09-23 17:37:14 +0530

    django app hosting

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +86,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
